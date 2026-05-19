31f1a2f4941277ea12786bbeea5096e293e77861
Nikhil Tanwar <nikhil@DESKTOP-FL4OOC7.localdomain>
2026-05-19 12:21:03 +0000

    first commit

---
+++ assaignment.py
@@ -0,0 +1,125 @@
+# name = "Nikhil Tanwar"
+# age = 19
+# course = "BTECH"
+
+# print("Name",name)
+# print("age",age)
+# print("course",course)
+
+
+
+# Write a program to print the name, age, and course details of three students.
+#  All the details should be stored in separate variables and then print
+
+# name1 = "rahul"
+# age1 = "15"
+# course1 = "btech"
+
+# name2 = "bihari"
+# age2 = "18"
+# course2 = "bsc"
+
+# name3 = "mayur"
+# age3 = "19"
+# course3 = "BBA"
+
+# print("student name :",name1)
+# print("age",age1)
+# print("course",course1)
+
+# print("student name :",name2)
+# print("age",age2)
+# print("course",course2)
+
+# print("student name :",name3)
+# print("age",age3)
+# print("course",course3)
+
+# names = ["mayur","bihari","piyush"]
+# ages = [16,17,18]
+# courses = ["btech","bca","bsc"]
+
+# for i in range(3):
+#     print("student",i+1)
+#     print("name",names[i])
+#     print("age",ages[i])
+#     print("course",courses[i])
+#     print()
+
+a=1
+b=2
+
+# a = a+b 
+# print("sum =",a)
+
+# a = float(input("enter the number"))
+# b = float(input("enter the second number"))
+
+# c = a*b
+# print("multiply of a and b is",c)
+
+# 5
+
+# ch = input("enter a character: ")
+# print("ASCHI value =",ord(ch)) # ord (gives the asii value of character )
+
+# 6th question
+
+# a = int(input("enter the first number "))
+# b = int(input("enter the second number"))
+
+# print("sum=",a + b)
+
+
+# 7th que 
+# a = int(input("enter the first number: "))
+# b = int(input("enter the second number: "))
+# print("the diffrence is =",a - b)
+
+ # que 8
+
+# a = int(input("enter the first number: "))
+# b = int(input("enter the second number: "))
+# print("the multiplication of  =",a * b)
+
+# que 9
+
+# a = int(input("enter the first number: "))
+# b = int(input("enter the second number: "))
+# print("the division is =",a / b)
+
+
+# 10th question\\
+
+# 
+
+# 11th question\\
+
+# a = int(input("enter the first number: "))
+# b = int(input("enter the second number: "))
+# result = a % b
+# print("the reminder is =",result)
+
+
+# 12th question\\
+
+# a = int(input("enter the number: "))
+# b = int(input("enter the power: "))
+# result = a ** b
+# print("the power value of a is  =",result)
+
+
+# plant = input("enter plant namee: ")
+
+# if plant == "Spathiphylluon":
+#     print("yes - Spathiphyllum is the best plant ever!")
+
+# elif plant == "spathipyllon":
+#     print("No, I want a big Spathiphyllum")
+
+# else:
+#     print("Spathiphyllum! Not",plant + "!")
+
+
+for temp in range(1, 7):
+    print(str(temp)*temp)
---
+++ basic.py
@@ -0,0 +1,304 @@
+# print("hello bihari")
+# print("hello")
+
+# # myvaribleclasas
+# # my_name_varaible
+
+# age = 4
+# print(age)
+# print("age") #print same which is in double ""
+
+
+# name = "aditya"
+# profession = "Software devloper"
+# experience = "10 years"
+# print("hello, I am",name,". I am a ",profession,". I have",experience," experience with it")
+
+# x = 5 
+# print(x)
+# print(type(x))
+# x = " hello world "
+# print(x)
+# print(type(x))
+# x=20
+# print(x)
+# print(type(x))
+# x=20.5
+# print(x)
+# print(type(x))
+# x=1j
+# print(x)
+# print(type(x))
+# x = ["apple", "banana", "cherry"]
+# print(x)
+# print(type(x))
+# x = ("apple", "banana", "cherry")
+# print(x)
+# print(type(x))
+# x = range(6)
+# print(x)
+# print(type(x))
+# x = {"name": "John", "age": 36}
+# print(x)
+# print(type(x))
+# x = {"apple", "banana", "cherry"}
+# print(x)
+# print(type(x))
+# x = frozenset({"apple","banana","cherry"})
+# print(x)
+# print(type(x))
+# x = True
+# print(x)
+# print(type(x))
+# x = b"Hello"
+# print(x)
+# print(type(x))
+# x = bytearray(5)
+# print(x)
+# print(type(x))
+# x = memoryview(bytes(5))
+# print(x)
+# print(type(x))
+# x = None
+# print(x)
+# print(type(x))
+
+# # 10101010 >> => 10101010101
+
+# print(5 >> 2)
+
+# x = 1
+
+# print(x:=3)
+# print(x==1)
+# print(x==2)
+
+# x=4
+# print(x < 4 and x < 10)
+# print(x > 5 or x > 10)
+# print(x > 4 or x > 10)
+# print(not(x > 3 or x > 10 ))
+
+
+# x = [ "maruti,bmw"]
+# y = [ "maruti,bmw"]
+# z= x
+
+
+# # write a program to calcuate hypotaneous between to sides
+
+# a =  int ( input ("enter the hight of the traiangle" ))
+# b = int (input ("enter the hight of traingle" ))
+# a
+# h =(a ** 2 + b ** 2)**0.5
+# print("hypotaneous =",h)
+
+# # write a program to print the pattern using print function
+
+# print("+............+")
+# print("|            |")
+# print("|            |")
+# print("|            |")
+# print("|            |")
+# print("|            |")
+# print("+............+") 
+
+# print("+"+"-"*10+"+")
+# print(("|"+" "*10+"|\n")*5,end="")
+# print("+"+"-"*10+"+")
+
+                      # decision making in python
+
+
+# number1 = int(input("enter the first number"))
+# number2 = int(input("enter the second number"))      
+
+# if number1 > number2:
+#     large_num=number1
+
+# else:
+# print("the large num is ",large_num)
+                                        
+# number1 = int(input("enter the first number"))
+# number2 = int(input("enter the second number"))      
+# number3 = int (input("enter the third number"))
+# large_num=number3
+
+# if number1 > large_num:
+#     large_num
+
+
+                                        
+# number1 = int(input("enter the first number"))
+# number2 = int(input("enter the second number"))      
+# number3 = int(input("enter the third  number"))
+
+# small_num = min(number1,number2,number3)
+# large_num = max(number1,number2,number3)
+# print("largest no. is "large_num)
+# print("smallest no. is "small_num)
+
+# number1 = int(input("enter the first number"))
+# number2 = int(input("enter the second number"))      
+# number3 = int(input("enter the third  number"))
+
+# a = input("Enter the word : ")
+
+# if a == "Spathiphyllum" :
+#     print("no - Spathiphyllum is the best plant ever!")
+# elif(a=="SPATHIPHYLLUM"):
+#     print("Yes - Spathiphyllum is the best plant ever!")
+# elif(a=="input"):
+#     print("spathiphally")
+
+# while True:
+#     print("bihari")
+#     print("ji")
+
+
+# largest_num = -99999999
+
+# num = int(input("enter the number or type -bihari to stop"))
+
+# while number != -1:
+    # if number > largest_num:
+    #     largest_num = number
+    # num = int(input("enter the number or type -bihari to stop"))
+
+# number = int(input("enter the number "))
+
+# count = 1
+# while count <= number:
+#     print(count ,end=" ")
+#     count+=1
+
+# write a program that reads a sequence of num and counts
+
+# number = int(input("enter the number "))
+# even = 0
+# odd = 0
+# count = 1
+# while count <=number:
+#     if count % 2 == 0:   
+#         even +=1
+#     else:
+#         odd+=1
+
+#     count+=1
+          
+
+# print("even=" , even)
+# print("odd=" , odd)
+
+
+#for counter in range(100):
+ #  print("counter", counter)
+
+            # KARMA IS A BITCH 
+#for counter in range(2,7,3):
+ #   print("the value of counter is currently: ",counter)
+
+# power = 1 
+# for expo in range(16):
+#     print("2 ton nthe oiwer of",expo,"is",power)
+#     power *= 2
+
+# print("the break ineterprator")
+# for counter in range(1,6):
+#     if counter ==3:
+#         break
+
+#     print("inside the loop",counter)
+# print("outside the loop")
+
+# print("the break ineterprator")
+# for counter in range(1,6):
+#     if counter ==3:
+#         continue
+
+#     print("inside the loop",counter)
+# print("outside the loop")
+
+# var = 77
+# print(var  > 0)
+# print(var <= 0)
+
+
+# num = [20,3,3,4,3]
+# print(num)
+# print(type (num))
+
+
+# numbers = [10,5,7,2,1]
+
+# print("first element content",numbers[0])
+# print("first element content",numbers[1])
+# print("first element content",numbers[2])
+# print("first element content",numbers[3])
+# print("first element content",numbers[4])
+
+# numbers[0]= 111
+# print("new list contents",numbers)
+
+# del numbers[3]
+# print(len(numbers))
+# print(numbers)
+
+
+# print("first element content",numbers[-1])
+# print("first element content",numbers[-2])
+# print("first element content",numbers[-3])
+# print("first element content",numbers[-4])
+
+# hat = [1,2,3,4,]
+# print(len(hat))
+# del hat[len(hat)-1]
+# print(hat)
+# hat[-1]= 11
+# print(hat)
+
+
+# list = [1,2,3,4,5]
+# print(list)
+# list.append(2)
+# print(list)
+
+# list.insert(0,6)
+# print(list)
+# del list[1]
+# print(list)
+
+# mylist = [1,2,3,4,5,6,7,8,9,10]
+# for iterator in range(len(mylist)):
+#     print(mylist[iterator])
+
+
+
+
+# list = []
+
+# for iterator in range(11):
+#     list.append(iterator+1)
+
+
+# print(list)
+
+
+# 
+# list = [10,30,30,40,50,60]
+# total = 0
+# for i in list:
+#     total = total + i
+
+# print("sum of all the elements =",total)
+
+
+# var1 = 1
+# var2 = 2
+# print("variable",var1)
+# print("variable",var2)
+
+list = [10,30,30,40,50,60]
+list[4], list[1] = list[1], list[4]
+
+print[list]
