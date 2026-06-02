4269542a590ab808039cda56f31de448674a2bfe
NIKHIL TANWAR <nikhilstanwar48@gmail.com>
2026-06-02 18:08:33 +0530

    inheritance

--- basic.py
+++ basic.py
@@ -606,29 +606,131 @@ board = []
 # # self.age = 20
 # # self.gender = "Male"
 # # self.grade = "12th"
-# self.printdetails()
-# # print(self.name)
-# # print(self.age)
-# # print(self.gender)
-# # print(self.grade)
+# # self.printdetails()
+# # # print(self.name)
+# # # print(self.age)
+# # # print(self.gender)
+# # # print(self.grade)
 
 
 
-class ExampleClass:
-    def __init__(self,val = 1):
-        self.first = val
+# # class ExampleClass:
+# #     def __init__(self,val = 1):
+# #         self.first = val
 
-    def set_second(self, val):
-        self.second = val 
+# #     def set_second(self, val):
+# #         self.second = val 
 
-example_object_1 = ExampleClass()
-example_object_2 = ExampleClass(2)
-example_object_2.set_second(3)
-example_object_3 = ExampleClass(4)
-example_object_3.third = 5 
+# # example_object_1 = ExampleClass()
+# # example_object_2 = ExampleClass(2)
+# # example_object_2.set_second(3)
+# # example_object_3 = ExampleClass(4)
+# # example_object_3.third = 5 
 
-print(example_object_1)
+# # print(example_object_1)
 
-print(example_object_1.__dict__)
-print(example_object_2.__dict__)
-print(example_object_3.__dict__)
+# # print(example_object_1.__dict__)
+# # print(example_object_2.__dict__)
+# # print(example_object_3.__dict__)
+
+# # class classy:
+# #     def method(self, par):
+# #         print("method", par)
+
+
+# # obj = classy()
+# # obj.method(50)
+
+# # class classy:
+# #     varia = 2
+# #     def method(self):
+# #         print(self.varia, self.var)
+
+# # obj = classy()
+# # obj.var = 3
+# # obj.method()
+
+# class csk:
+#     def __init__(self, thala, raina):
+#         self.thala = thala
+#         self.raina = raina
+
+# Mahi = csk("Mahi","Dhoni")
+# print(Mahi)
+# print("chennai super kings")
+
+# class star:
+#     def __init__(self, name, galaxy):
+#         self.name = name
+#         self.galaxy = galaxy
+
+#     def __str__(self):
+#         return self.name + ' in ' + self.galaxy
+    
+# sun = star("sun","milky way")
+# print(sun)
+
+# class shop:
+#     pass
+
+# class wafers(shop):
+#     pass
+
+# class chips(wafers):
+#     pass
+# for cls1 in (shop, wafers, chips):
+#     for cls2 in (shop, wafers, chips):
+#         print(issubclass(cls1,cls2), end="\t")
+#     print()
+
+# class Nikhil:
+#     subVar = 7
+
+# class
+# print(obj.subVar Teshuji(Nikhil):
+#     supVar = 14
+
+# obj = Teshuji()
+# print(obj.supVar))
+
+class Super:
+    def __init__(self):
+        self.supVar = 11 
+class Sub(Super):
+    def __init__(self):
+        super() . __init__ ()
+        self.subVar = 12
+
+obj = Sub()
+print(obj.subVar)
+print(obj.supVar)
+
+class Level1:
+    variable_1 = 100
+    def __init__(self):
+        self.var_1 = 101
+    def fun_1(self):
+        return 102
+    
+class Level2(Level1):
+    variable_2 = 200
+    def __init__(self):
+        super().__init__()
+        self.var_2 = 201
+
+    def fun_2(self):
+        return 202
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
+
+    def fun_3(self):
+        return 302
+    
+obj = Level3()
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
+
